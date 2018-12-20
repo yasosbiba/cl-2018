1cd15f04fdf5ab19050716ea9f487d7bff9ac423
yasosbiba <pynya.vlad@yandex.ru>
2018-12-21 00:30:50 +0300

    typeahead and server changes

--- server.py
+++ server.py
@@ -7,11 +7,10 @@ import requests, re
 from flask import Flask
 from flask import render_template
 from flask import request
-
+from flask import abort
 
 app = Flask(__name__)
 
-#m = 'ruscorpora_upos_skipgram_300_5_2018.vec.gz'
 m = 'shakespeare.model'
 model = Word2Vec.load(m)
 #similar = model.most_similar('человек_NOUN')
@@ -31,20 +30,37 @@ def api_neighbor(m, w, f):
             continue
     return neighbors
 
+
+def api_similarity(m, w1, w2):
+    url = '/'.join(['http://rusvectores.org', m, w1 + '__' + w2, 'api', 'similarity/'])
+    r = requests.get(url, stream=True)
+    return r.text.split('\t')[0]
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
 
-@app.route('/search')
-def show_similar():
-    word = request.args.get('word', '')
-    res = model.most_similar(word)
-    #res = request.form['word']
-    return render_template('show_similar.html', res=res)
+@app.route('/description')
+def description():
+    return render_template('description.html')
 
-@app.route('/rusvectors')
-def show_similar_rv():
-    WORD = request.args.get('word-wv', '')
-    res = api_neighbor(MODEL, WORD, FORMAT)
-    return render_template('show_similar_rv.html', res=res)
-    
+@app.route('/search/synonyms')
+def show_synonym():
+    word = request.args.get('word', '')
+    try:
+        res = model.most_similar(word)
+    except:
+        abort(404)
+    res_rv = api_neighbor(MODEL, word, FORMAT)
+    return render_template('show_synonyms.html', res=res, res_rv=res_rv)
+
+@app.route('/search/similarity')
+def show_similar():
+    words = request.args.get('word', '').split()
+    res = model.most_similar(words[0], words[1])
+    res_rv = api_similarity(MODEL, words[0], words[1])
+    return render_template('show_similar.html', res=res, res_rv=res_rv)
