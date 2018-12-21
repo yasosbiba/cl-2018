adf2ed3b5598f1891e91daa82c0b3374d650565b
yasosbiba <pynya.vlad@yandex.ru>
2018-12-21 04:50:00 +0300

    changed learning parameters

--- learning.py
+++ learning.py
@@ -2,16 +2,25 @@
 import sys
 import gensim, logging
 import ast
+import os
+from gensim.models import Word2Vec
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-from gensim.models import word2vec
-from gensim.test.utils import get_tmpfile
+curdir = os.getcwd()
 
-f = open('сонеты_обработанные.txt', 'r', encoding='utf-8')
-data = ast.literal_eval(f.read())
-f.close()
+def create_corpus(corpus):
+    for filename in os.listdir(curdir + '/data/processed/'):
+        #open our text
+        f = open(curdir + '/data/processed/' + filename, 'r', encoding='utf-8')
+        t = ast.literal_eval(f.read())
+        for s in t:
+            corpus.append(s)
+        f.close()
 
-model = word2vec.Word2Vec(data, min_count=5, size=50, window=5, workers=4, sg=1, hs=1)
+corpus = []
+create_corpus(corpus)
+
+model = Word2Vec(corpus, min_count=20, size=100, window=15, workers=4, sg=0, hs=1, iter=10)
 model.save('shakespeare.model')
-print(model.most_similar('глаз_NOUN'))
+#print(model.most_similar('любовь_NOUN'))
